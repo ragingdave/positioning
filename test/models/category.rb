ab7612e6d0ad79d230ede6910ee75db8e3d15e55
Nikita Popov <nikita.luft89@gmail.com>
2025-08-28 22:53:23 +0500

    ST-585: Removed optional: true from Post

--- test/models/category.rb
+++ test/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  belongs_to :parent, class_name: "Category" # Removed `optional: true` to enable compatibility
+  belongs_to :parent, class_name: "Category" # Removed `optional: true` to enable compatibility and make the tests run
 
   positioned on: :parent
 end
