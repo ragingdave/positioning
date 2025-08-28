092b5b7201ebc78a89df8bb4fd7d6beda07ce3e2
Nikita Popov <nikita.luft89@gmail.com>
2025-08-28 22:46:31 +0500

    ST-585: Removed optional: true from Category model to enable compatibility

--- test/models/category.rb
+++ test/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  belongs_to :parent, class_name: "Category", optional: true
+  belongs_to :parent, class_name: "Category" # Removed `optional: true` to enable compatibility
 
   positioned on: :parent
 end
